9f8936140b22092aef6a1659af1430e0049c333f
nunofaria2 <nunofaria855@gmail.com>
2024-04-22 10:22:22 +0100

    .

--- controllers/carros.js
+++ controllers/carros.js
@@ -44,15 +44,45 @@ exports.create = async (req, res) => {
 //atualiza o carro
 exports.update = async (req, res) => {
     //obter o carro pelas características enviadas
-    const {number, name, city, birthday } = req.body;
-    //envia o carro alterado
-    return res.send(req.body);
+    const {id, Marca, Detalhes, Foto} = req.body;
+     //ler o ficheiro local
+     const datajson = fs.readFileSync("data/local/data.json", "utf-8");
+     //parse do json
+     const data = JSON.parse(datajson);
+     //procurar o carro para actualizar
+    const carros = data.carros.find(carro => carro.id == id);
+    //atualizar as caraterísticas
+    carros.Marca = Marca;
+    carros.Detalhe = Detalhes;
+    carros.Foto = Foto;
+    //actualizar no ficheiro json
+    fs.writeFileSync('data/local/data.json', JSON.stringify(data));
+    //devolver o carro alterado
+    return res.send({id, Marca, Detalhe, Foto});
 }
+
 
 //apaga o carro com o id
 exports.delete = async (req, res) => {
     //obter o id do carro
     const id = req.params.id;
-    //devolve ok
-    return res.send("ok");
+     //ler o ficheiro local
+     const datajson = fs.readFileSync("data/local/data.json", "utf-8");
+     //parse do json
+     const data = JSON.parse(datajson);
+     //procurar o indice do carro a ser procurada
+    const carroIndex  = data.carros.findIndex(carro => carro.id == id);
+     // Verifique se o carro foi encontrado
+    if (carroIndex !== -1) {
+        // Exclua o estudante do array de estudantes
+        const apagaCarro = data.carros.splice(carroIndex, 1)[0];
+        // Atualize o ficheiro json
+        fs.writeFileSync('data/local/data.json', JSON.stringify(data));
+        // Retorne o carro excluído como resposta
+        return res.status(200).send(apagaCarro);
+    } else {
+        // Caso o carro não seja encontrado, retorne uma resposta de erro
+        return res.status(404).send("Carro não encontrado");
+    }
 }
+
