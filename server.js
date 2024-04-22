2a7494b09b2170f5b88a91574b41917fc73962f1
nunofaria2 <nunofaria855@gmail.com>
2024-04-22 10:51:30 +0100

    .

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const cors = require('cors');
 const express = require('express');
 
 const router = require('./routes/index');
+const publicoRouter = require('./routes/publico');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/', publicoRouter);
 app.use('/api/', router);
 
 const port = process.env.SERVER_PORT || 8080;
